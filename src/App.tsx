718d0a530daa09d2b05844fe4146eda7f2d556fc
LitvinenkoBD <boris12lit@gmail.com>
2022-05-25 15:43:55 +0300

    added type for children in Item.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Item from './components/Item';
 
 const App = () => {
-  return (
-    <div>
-      <Item width='200px' height='250px'/>
-    </div>
-  );
+    return (
+        <div>
+            <Item width='200px' height='250px'>
+                <button>Wake Up</button>
+            </Item>
+        </div>
+    );
 };
 
 export default App;
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 interface ItemProps {
     width: string;
-    height?: string
+    height?: string;
+    children: JSX.Element[] | JSX.Element
 }
+// type React.ReactChild = string | number | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+// JSX.Element[] | JSX.Element
+// React.ReactElement | React.ReactNode
 
-const Item = ({width, height}: ItemProps) => {
+const Item: React.FC<ItemProps> = ({width, height, children}) => {
     return (
         <div style={{width, height, background: 'green'}}>
             Wake up, Neo...
+            {children}
         </div>
     );
 };
