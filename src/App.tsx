33e3f2b2994d8caaf80339b8cf66c70c4cf8a612
LitvinenkoBD <boris12lit@gmail.com>
2022-05-27 22:23:10 +0300

    added ItemList.tsx types.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Item, { VariantCollection } from './components/Item';
+import ItemList from './components/ItemList';
+import { IUser } from './types/types';
 
 const App = () => {
+    const [users, setUsers] = useState<IUser[]>([])
+
+    useEffect(()=>{
+        fetchUsers()
+    }, [])
+
+    async function fetchUsers () {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
     return (
-        <div>
+        <div style={{background: 'black', height: '100vh'}}>
             <Item width='200px' height='250px' variant={VariantCollection.bordered} onClick={(num)=>{alert(num)}}>
                 <button>Wake Up</button>
             </Item>
+            <ItemList users={users}/>
         </div>
     );
 };
---
+++ src/components/ItemList.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { IUser } from '../types/types';
+
+interface ItemListProps {
+    users: IUser[]
+}
+
+const ItemList: FC<ItemListProps> = ({users}) => {
+    // users[0].address.city
+    return (
+        <div>
+            {users.map(user => { 
+                return (
+                <div key={user.id} style={{padding: 10, border: '1px solid green', color: 'green'}}>
+                    {user.id} {user.name} live in {user.address.city} on {user.address.street} street 
+                </div>
+                )                
+            }
+            )}
+        </div>
+    );
+};
+
+export default ItemList;
