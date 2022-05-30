d5cfdedfc1891f158ba71533b01d08e058796707
LitvinenkoBD <boris12lit@gmail.com>
2022-05-30 19:06:54 +0300

    added useRef logic to  EventsExample.tsx

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,13 +1,19 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 
 const EventsExample: FC = () => {
     const [value, setValue] = useState<string>("")
     const [isDrag, setIsDrag] = useState<boolean>(false)
+    const inputRef = useRef<HTMLInputElement>(null)
+
+
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
     }
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        alert(value)
+        //@ts-ignore:next-line
+        alert(inputRef.current.value)
+
+        // alert(inputRef.current?.value) with "?" doesn't work
     }
     const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
         // e.dataTransfer
@@ -30,7 +36,10 @@ const EventsExample: FC = () => {
 
     return (
         <div>
-            <input type="text" value={value} onChange={changeHandler} />
+            <input type="text" value={value} onChange={changeHandler} placeholder="Controlled input"/>
+            <br/>
+            <input type="text" ref={inputRef} placeholder="Out of Control input"/>
+            <br/>
             <button onClick={clickHandler}>click Handler</button>
             <div onDrag={dragHandler} draggable style={{ width: 320, height: 100, background: "green", margin: 20 }}>DnD 1</div>
             <div
